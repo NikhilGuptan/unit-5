44593e7f7d770c69529c022fee5c3a5f5bbf7ab6
Nikhil Gupta <ng3152340@gmail.com>
2021-10-22 19:12:48 +0530

    intro-5

---
+++ week-3/day-4/you/ts-react/src/Components/Button.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Button = ({ label ="", onClick = () =>{}  }) => {
+  return (
+    <button data-testid="button" onClick={() => onClick()}>
+      {label}
+    </button>
+  )}
